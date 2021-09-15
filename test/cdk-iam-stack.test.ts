c13ce8ef2880b620e3c5d394da9a67fc93f9747c
abdulrafayghani <abdulrafay.ghani@gmail.com>
2021-09-15 12:31:19 +0500

    Initial commit

---
+++ test/cdk-iam-stack.test.ts
@@ -0,0 +1,11 @@
+import * as cdk from 'aws-cdk-lib';
+import * as CdkIamStack from '../lib/cdk-iam-stack-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkIamStack.CdkIamStackStack(app, 'MyTestStack');
+    // THEN
+    const actual = app.synth().getStackArtifact(stack.artifactId).template;
+    expect(actual.Resources ?? {}).toEqual({});
+});
